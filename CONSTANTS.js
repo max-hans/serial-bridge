506272da7a222368caa340e55b2c7a06774adc7d
max <m.hans@me.com>
2023-05-13 18:46:54 +0200

    clean

--- CONSTANTS.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RECONNECT_INTERVAL = exports.BAUD_RATE = void 0;
-exports.BAUD_RATE = 115200;
-exports.RECONNECT_INTERVAL = 500;
